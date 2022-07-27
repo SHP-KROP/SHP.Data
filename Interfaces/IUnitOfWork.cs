860cfd8873eb308ccfbbb760327a08faf25a003a
Oleksandr Bondarenko <oleksandr.bondarenko@blueprism.com>
2022-07-27 14:39:15 +0300

    Add project files.

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Repositories;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository ProductRepository { get; }
+
+        ICategoryRepository CategoryRepository { get; }
+
+        IUserRepository UserRepository { get; }
+
+        ISignInManager SignInManager { get; }
+
+        Task<bool> ConfirmAsync();
+    }
+}
